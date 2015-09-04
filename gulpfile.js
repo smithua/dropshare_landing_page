19984f5cd0316cf9a55a6b070bba52bfa98d95b6
smithua <smithuaa@gmail.com>
2015-09-04 16:44:07 +0300

    made build process much easier

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 var
     gulp            = require('gulp'),
     sass            = require('gulp-sass'),
-    inline          = require('gulp-inline'),
     postcss         = require('gulp-postcss'),
     autoprefixer    = require('autoprefixer'),
     mqpacker        = require('css-mqpacker'),
-    csswring        = require('csswring'),
     gulpSequence    = require('gulp-sequence'),
     del             = require('del'),
-    browserSync     = require('browser-sync');
+    browserSync     = require('browser-sync'),
+    critical        = require('critical').stream;
 
 
 //////////////////////////////////////////////////
@@ -16,55 +15,57 @@ var
 //////////////////////////////////////////////////
 
 gulp.task('sass', function () {
-  return gulp.src('./*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    return gulp.src('./*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('postcss', function () {
     var processors = [
         autoprefixer({browsers: ['last 2 version']}),
-        mqpacker,
-        csswring
+        mqpacker
     ];
     return gulp.src('./css/*.css')
         .pipe(postcss(processors))
-        .pipe(gulp.dest('./test/'))
+        .pipe(gulp.dest('./build/'))
         .pipe(browserSync.reload({
           stream: true
         }));
 });
 
-// INLINE CSS INTO HTML
+// Generate & Inline Critical-path CSS/JS
 gulp.task('inline', function () {
     return gulp.src('./*.html')
-      .pipe(inline({
-        disabledTypes: ['svg', 'img', 'js']
-      }))
-      .pipe(gulp.dest('./'));
+        .pipe(critical({
+            base: './build/',
+            inline: true,
+            minify: true,
+            css: ['./build/main.css']
+        }))
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('browserSync', function() {
-  browserSync({
-    server: {
-      baseDir: './test/',
-      index: "Dropshare-LandingPage.html"
-    },
-  })
-})
+    browserSync({
+        server: {
+            baseDir: './',
+            index: "Dropshare-LandingPage.html"
+        }
+    })
+});
 
+// CLEAN task (remove folders/files)
 gulp.task('clean', function () {
     return del(['./css']);
-})
+});
 
-gulp.task('query', gulpSequence('sass', 'postcss', 'clean'));
-gulp.task('query2', gulpSequence('sass', 'postcss', 'inline', 'clean'));
-
-// watch task
+// WATCH task
 gulp.task('watch', ['browserSync'], function (){
     gulp.watch('*.scss', ['sass']);
-    gulp.watch('./css/*.css', ['postcss']);
-})
+    gulp.watch('css/*.css', ['postcss']);
+});
 
 // DEFAULT task
 gulp.task('default', ['watch']);
+// BUILD task
+gulp.task('build', gulpSequence('sass', 'postcss', 'inline'));
